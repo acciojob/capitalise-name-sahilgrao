c59f8d5dc533f8696ecb8db60cff39782aecb615
Sahil G Rao <92579906+sahilgrao@users.noreply.github.com>
2026-02-22 11:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function covertToUppercase(){
+	// get the input element
+	let inputField = document.getElementById("fname");
+
+	//convert its value to uppercase
+	inputField.value = inputField.value.toUpperCase();
+}
